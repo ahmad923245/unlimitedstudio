ae77531ff2a9d65082b73ba54e3f832330db0f9c
Sami ullah <adilasif502@gmail.com>
2023-09-24 23:26:16 +0500

    change to 3.9

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo " BUILD START"
-python -m pip install -r requirement.txt
-python manage.py collectstatic --noinput --clear
-python manage.py runserver 
+python3.9 -m pip install -r requirement.txt
+python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py runserver 
 echo " BUILD END"
