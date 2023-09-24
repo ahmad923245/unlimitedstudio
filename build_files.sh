6c389047f29ad8cb338738b048c5af3a9381de8f
Sami ullah <adilasif502@gmail.com>
2023-09-24 23:25:07 +0500

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo " BUILD START"
-python3.8 -m pip install -r requirement.txt
-python3.8 manage.py collectstatic --noinput --clear
-python3.8 manage.py runserver --noinput --clear
+python -m pip install -r requirement.txt
+python manage.py collectstatic --noinput --clear
+python manage.py runserver 
 echo " BUILD END"
