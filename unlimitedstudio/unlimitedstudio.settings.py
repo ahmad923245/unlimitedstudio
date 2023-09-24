d600687f793d68f7b6a8ff408e4aa40f83bdb38e
Sami ullah <adilasif502@gmail.com>
2023-09-24 12:46:35 +0500

    done

--- unlimitedstudio/unlimitedstudio.settings.py
+++ unlimitedstudio/unlimitedstudio.settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v0h!u1#176+u4sbdfolkqe33gcn9ht&t66*@-*jma#-_^p#g9j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
